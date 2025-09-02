32b84fa75ea5460f3d154e58c582c177d06c7911
dinolang1 <dino.lang03@gmail.com>
2025-09-02 16:38:29 +0200

    Privremeno isključen secure flag za session cookie (debug Render)

--- server/index.ts
+++ server/index.ts
@@ -42,7 +42,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: NODE_ENV === 'production',
+    secure: false, // privremeno za testiranje na Render-u
     httpOnly: true,
     maxAge: 24 * 60 * 60 * 1000, // 24 hours
   },
